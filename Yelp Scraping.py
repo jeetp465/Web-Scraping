6220f5141414112b05f616e6b312482b9a65b6de
Jeet Patel <jeet.patel022@gmail.com>
2022-03-03 13:18:14 -0800

    Added comments to the code. To do: Fix the bug in loop.

--- Yelp Scraping.py
+++ Yelp Scraping.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
-# coding: utf-8
 
-# In[32]:
-
-
-import pandas as pd
+# Loading the required libraries
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -12,80 +8,83 @@ import random
 from tqdm.notebook import tqdm
 import re
 
-
-# In[8]:
-
-
+# Declaring the pages variable to iterate over for fetching top 40 results
 page_var = ['0','10','20','30']
 base_url = 'https://www.yelp.com/search?find_desc=donut+shop&find_loc=San+Francisco%2C+CA+94105&start='
+
+# Base filename to save the search pages
 base_fname = 'sf_donut_shop_search_page_'
+
+# Headers to pass with the GET request
 headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}
 
-
-# In[19]:
-
-
+# Looping over page_var and fetching top 40 Donut Shops in San Francisco on Yelp 
 page_ids = []
 for page in tqdm(page_var):
+    # Modifying the search url
     search_url = base_url + page
     
+    # Creating page_ids from the page search term
     page_id = str(int(int(page)/10 + 1))
     page_ids.append(page_id)
     
+    # Fetching the search page for Donut Shops
     search_response = requests.get(search_url, headers = headers)
     search_content = str(search_response.content)
     
+    # Defining filename to save the webpage
     fname = base_fname + page_id + '.html'
     
     with open(fname, 'w') as f:
         f.write(search_content)
     f.close()
     
+    # Adding sleep time to mimic human browsing and avoid getting blocked from Yelp. 
+    # Don't want to go through the trouble of requesting for a new IP address
     time.sleep(random.randint(3,6))
 
-
-# In[20]:
-
-
-page_ids
-
-
-# In[21]:
-
-
+# Working with a single search page for now. 
+# Will add a loop later to iterate over all the pages. 
 fname = base_fname + page_ids[0] + '.html'
-fname
-
-
-# In[22]:
-
 
 with open(fname, 'r') as f:
+    # Parsing the saved file into html
     soup = BeautifulSoup(f, "html.parser")
 f.close()
 
-
-# In[81]:
-
-
+# Defining a url prefix to append to the shop urls
 url_prefix = 'https://www.yelp.com'
+
+# Identifying all the titles on the search page
 title = soup.find_all('span', class_ = 'css-1uq0cfn')
+
+# Looping over all the identified titles
 for elem in title:
+    # Differentiating the result as per recommended sorting from the 
+    # sponsored result.
+    
     if elem.text[0].isdigit():
+        # Cleaning the label and extracting shop rank and shop name from it.
         clean_label = re.sub(r'\\[a-z0-9]{3}','',elem.text)
         shop_rank = clean_label.split('.')[0]
         shop_name = clean_label.split('.')[1]
         
+        # Extracting the shop url and appending the url prefix defined earlier
         url = elem.find('a')['href']
         shop_url = url_prefix + str(url)
         
+        # Identified the common siblings to go to the next line for fetching
+        # number of reviews and the rating of the shop
         sibling = elem.find_parent('div').find_parent('div').find_parent('div').find_next_sibling()
         
         reviews = sibling.find('span', class_ = 'reviewCount__09f24__tnBk4 css-1e4fdj9').text
         ratings = sibling.find('div', class_ = "i-stars__09f24__foihJ i-stars--regular-4__09f24__zkZZV border-color--default__09f24__NPAKY overflow--hidden__09f24___ayzG")['aria-label']
         
+        # Fetching the next sibling to identify the price range and the tags associated with a shop
         sibling = sibling.find_next_sibling()
 
+        # Dynamically maintaing tags.
+        # Need to account for no tags. 
         tags = sibling.find_all('a')
         tags_list = []
     
@@ -93,6 +92,8 @@ for elem in title:
             tag = item.find('p').text
             tags_list.append(tag)
             
+        # Fetching the price range mentioned as '$', '$$', '$$$' for the shops
+        # Try-Catch loop to account for missing '$'*
         try:    
             price_range = sibling.find('span', class_ = 'priceRange__09f24__mmOuH css-18qxe2r').text
         except:
@@ -136,10 +137,3 @@ for elem in title:
         print (f'Order Yelp: {order_yelp}')
         
         print('--------------------------------')
-
-
-# In[ ]:
-
-
-
-
